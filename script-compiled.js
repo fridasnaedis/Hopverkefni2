f93d2f815f75da45af700f79372ec481cee5349b
Pallur <palliasgeir@gmail.com>
2017-11-21 16:38:49 +0000

    json ves

--- script-compiled.js
+++ script-compiled.js
@@ -1,6 +1,3 @@
-"use strict";
-<<<<<<< HEAD
-=======
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -9,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var videos = new Videos();
   videos.load();
 });
->>>>>>> 6564dcd7b050b672f695a638fe497c2e10567836
+"use strict";
 'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
