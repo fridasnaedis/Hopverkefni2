19d36f2bd3abec91a4fd6b3d1fbe16efd65c1e21
sakkattack <isakkolbeins@gmail.com>
2017-11-21 14:38:17 +0000

    shit drasl

--- script-compiled.js
+++ script-compiled.js
@@ -1,5 +1,13 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('I AM KET');
+  //console.log('Wowsa');
+  var videos = new Videos();
+  videos.load();
+});
+'use strict';
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -65,13 +73,5 @@ var Videos = function () {
 
   return Videos;
 }();
-'use strict';
-
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('I AM KET');
-  //console.log('Wowsa');
-  var videos = new Videos();
-  videos.load();
-});
 
 //# sourceMappingURL=script-compiled.js.map
