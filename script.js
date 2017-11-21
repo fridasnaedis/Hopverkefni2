126475974b37f056dca660cbd414fb0074c3638c
fridasnaedis <fridasnaedis@Frias-MacBook-Pro.local>
2017-11-21 02:02:49 +0000

    Vinna í script.js en það virkar samt ekki :)

--- script.js
+++ script.js
@@ -3,4 +3,50 @@ class Videos {
     this.cardlists = document.querySelector('.cardlists');
     this.player = document.querySelector('.player');
   }
+
+
+
+  load() {
+    console.log('Testy test :)');
+    const request = new XMLHttpRequest();
+    request.open('GET', './videos.json',true);
+
+
+    request.onload = function() {
+      const result = JSON.parse(request.response);
+      createCategories(result);
+    };
+
+    request.send();
+
+
+  }
+
+  createCategories(data) {
+    for(let i = 0; i < data.categories.length; i++){
+      console.log(data.categories[i].title)
+      const categoryTitle = data.categories[i].title;
+      const categoryVideos = data.categories[i].videos;
+      const category = document.createElement('div');
+      category.classList.add('.cardlist');
+      const titleNode = document.createElement('h2');
+      titleNode.appendChild(document.createTextNode(categoryTitle));
+      category.appendChild(titleNode);
+      //createVideos(currCategory);
+
+      this.cardlists.appendChild(category);
+    }
+  }
+
+
 }
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Wowsa');
+  const videos = new Videos();
+  videos.load();
+});
